d0520c558366a755f4485b6f3cf99f82771a4935
Geovanie I <100096209+geovie19@users.noreply.github.com>
2022-03-01 16:02:19 -0800

    Update sona.sh

--- sona.sh
+++ sona.sh
@@ -33,6 +33,7 @@ echo "Connect to the SonarQube server through the browser. It uses port 9000"
 echo "You can find your IP address below and copy and paste it in your browser with :9000"
 yum install net-tools -y 
 echo
+fi
 ifconfig | grep 192
 echo "copy and paste your IP address in your browser this way: ip-address:9000"
 echo "Login in SonarQue by using : User admin and Password admin"
